0bc2936c40f48481920fe8a0b8dee8a09cf2a4e2
bjclark13 <bjclark13@outlook.com>
2020-01-27 21:55:47 -0500

    Dumb BJ

---
+++ star-wars-crawler/src/app/film-detail-block/film-detail-block.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-film-detail-block',
+  templateUrl: './film-detail-block.component.html',
+  styleUrls: ['./film-detail-block.component.css']
+})
+
+export class FilmDetailBlockComponent implements OnInit {
+  @Input()
+  film : any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
